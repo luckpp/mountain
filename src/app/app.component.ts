1b46105323e625008cc09f86ed5cd3e00e8960dc
luckpp <marius.andritoi@yahoo.com>
2020-01-10 13:32:08 +0200

    added vertical small menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mountain';
+
+  showVericalMenuHandler(status: boolean) {
+    console.log(`showVericalMenuHandler(${status})`);
+  }
 }
--- src/app/components/menu/menu/menu.component.ts
+++ src/app/components/menu/menu/menu.component.ts
@@ -1,22 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { PageTypes } from '../../../model/page-types.model';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
 export class MenuComponent implements OnInit {
 
+  @Output() showVericalMenuChanged: EventEmitter<boolean>;
+
+  verticalMenuVisible: boolean;
+
   constructor(
     private _router: Router
-  ) { }
+  ) { 
+    this.showVericalMenuChanged = new EventEmitter<boolean>();
+    this.verticalMenuVisible = false;
+  }
 
   ngOnInit() {
   }
 
   navigateTo(pageType: string) {
     this._router.navigateByUrl(`/${pageType}/dashboard`);
+  }
+
+  vericalNavigateTo(pageType: string) {
+    this.navigateTo(pageType);
+    this.verticalMenuVisible = false;
+  }
+
+  showVericalMenuClicked() {
+    this.verticalMenuVisible = !this.verticalMenuVisible;
+    this.showVericalMenuChanged.emit(this.verticalMenuVisible);
+  }
+
+  onResize(event){
+    if (event.target.innerWidth > 770) {
+      if (this.verticalMenuVisible) {
+        this.verticalMenuVisible = false;
+      }
+    }
   }
 }
